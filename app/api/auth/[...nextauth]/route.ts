07cf802333273767ec37864de886f709be6e36ca
oktayudha05 <oktayudha05@gmail.com>
2024-08-07 22:02:20 +0700

    build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from "next-auth";
-import googleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  providers: [
-    googleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "@/libs/auth"; 
 
 const handler = NextAuth(authOptions);
 
