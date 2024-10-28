58f9fedb1eb217c169a946de5df9040a0e0a099c
oktayudha05 <oktayudha05@gmail.com>
2024-10-28 21:00:46 +0700

    bersihin sampah

--- app/api/espServer/route.ts
+++ app/api/espServer/route.ts
@@ -63,32 +63,3 @@ export async function GET(req: NextRequest) {
   }
 }
 
-
-
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function GET(req: NextRequest) {
-//   const { searchParams } = new URL(req.url);
-//   const state = searchParams.get("state");
-
-//   if (!state) {
-//     return NextResponse.json({ message: "No state provided" }, { status: 400 });
-//   }
-
-//   try {
-//     const esp32Url = `http://192.168.55.179/mesin?state=${state}`;
-//     const response = await fetch(esp32Url);
-
-//     if (!response.ok) {
-//       throw new Error(`Failed to set LED state. Status: ${response.status}`);
-//     }
-
-//     return NextResponse.json({ message: `LED is ${state}` });
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return NextResponse.json(
-//       { message: "Error controlling LED" },
-//       { status: 500 }
-//     );
-//   }
-// }
